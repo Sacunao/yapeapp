4e1275d003714a1624ec0a9e319feaa6c507d960
sacunao <sheila.aobregon@gmail.com>
2016-12-05 17:33:54 -0500

    agregando validación

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,29 @@
-var email = documennt.getElementById("email");
-var password = document.getElementById("password");
+var txtEmail = documennt.getElementById("email");
+var txtPassword = document.getElementById("password");
 var btnLogin = document.getElementById("login");
 
 btnLogin.addEventListener("click", function(e){
     e.preventDefault;
+    var email = txtEmail.value;
+    var pass = txtPassword.value;
+    auth().signInWithEmailAndPassword(email,password)
+          .catch(function(error){
+                  // Handle Errors here.
+                  var errorCode = error.code;
+                  var errorMessage = error.message;
+                  // ...
+            });;
     
 });
-//firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-//  // Handle Errors here.
-//  var errorCode = error.code;
-//  var errorMessage = error.message;
-//  // ...
+
+//
+//var contactTemplate = "";
+//    
+//
+//$(document).ready(function(){
+//    $.getJSON("http://localhost:3000/users.json", 
+//        function(response) {
+//            $("#···").html(plantilla
+//                             .replace("__nombre__", response.nombre)                   .replace("__apellido__", response.apellido));
+//        });
 //});
-
-
-
-var contactTemplate = "";
-    
-
-$(document).ready(function(){
-    $.getJSON("http://localhost:3000/users.json", 
-        function(response) {
-            $("#···").html(plantilla
-                             .replace("__nombre__", response.nombre)                   .replace("__apellido__", response.apellido));
-        });
-});
