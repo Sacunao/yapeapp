b61dc9bdb4c0a84292dbb0770c1cb85e4c35de0c
sacunao <sheila.aobregon@gmail.com>
2016-12-06 13:36:18 -0500

    prueba tranfer

--- public/js/payment.js
+++ public/js/payment.js
@@ -1,31 +1,27 @@
 $(document).ready(function(){
-    var userName = $("#nameUser").val();
-    var amount = $("#monto").val();
-    var message = $("#mensaje").val();
-//    var btnrequest = $("#btnrequest");
     
-    $("#btnrequest").click(function (userName, amount, message) {
+    $("#btnrequest").click(function(){
+//        var userName = $("#usuario").val();
+        var amount = $("#monto").val();
+        var message = $("#mensaje").val();
+        transfer(amount, message);
+    });
+
+    function transfer(amount, message){
+    
         // A post entry.
         var postData = {
-            username: userName,
+//            username: userName,
             monto: amount,
             mensaje: message
         };
-
         // Get a key for a new Post.
-        var newPostKey = firebase.database().ref().child('transfer').push().key;
-
+        var newPostKey = firebase.database().ref().child('/transfer').push().key;
         // Write the new post's data simultaneously in the posts list and the user's post list.
         var updates = {};
-        updates['/transfer/' + newPostKey] = postData;
-        updates['/transfer/' + "user1" + '/' + newPostKey] = postData;
+        updates['/transfer/' + newPostKey ] = postData;
+       // updates['/transfer/' + "user + "/"  + newPostKey] = postData;
         return firebase.database().ref().update(updates);
-        
-         console.log("userName");
-        
-    }); 
-    
-   
-    
-    
+    }
+ 
 });
