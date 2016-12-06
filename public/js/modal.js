2541f6b3bbd02e31520371be290274fc7c029b21
Sandy Ciquero <sandy.ciqueroch@gmail.com>
2016-12-06 08:29:02 -0500

    cambios en nombre

--- public/js/modal.js
+++ public/js/modal.js
@@ -2,8 +2,57 @@ $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
     
-    $("#close").click(function() {
-      window.location.href = "/init.html";
-//        console.log("hola");
+    var contactTemplate ="<div class='row'>"+"<div class='col s12 center-align lemarg'>"+
+            			  "<h3>Nombre: {{name}}</h3>"+"</div>"+"</div>";
+
+    var database = firebase.database();
+    
+    var users = firebase.database().ref('users/');
+
+    var contenedorData = "";
+    
+ //    var starCountRef = firebase.database().ref('users/' + );
+	// starCountRef.on('value', function(snapshot) {
+ // 	updateStarCount(postElement, snapshot.val());
+	// });
+
+    users.on('value', function(response) {
+    var data = response.val();
+    var la = $("titulo");
+    var contenedorData = "";
+    
+    $.each(data, function(i, user){
+            contenedorData += contactTemplate
+                     .replace("{{name}}", user.name)                   
+            
+        });
+        la.html(contenedorData);
+
+
     });
+
+	$("#close").click(function() {
+        window.location.href = "/init.html";
+       });
+ 
+
   });
+
+
+
+
+    
+    users.on('value', function(response) {
+    var data = response.val();
+    var la = $("#la");
+    var contenedorData = "";
+    
+    $.each(data, function(i, user){
+            contenedorData += contactTemplate
+                     .replace("{{name}}", user.name)                   
+            
+        });
+        la.html(contenedorData);
+
+
+    });
